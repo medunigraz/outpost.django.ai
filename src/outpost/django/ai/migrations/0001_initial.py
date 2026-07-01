456e35d55c2c658879bf669f1cbac256877242a4
Michael Fladischer <michael.fladischer@medunigraz.at>
2026-07-01 09:22:55 +0200

    Reset campusonline migration series.

--- src/outpost/django/ai/migrations/0001_initial.py
+++ src/outpost/django/ai/migrations/0001_initial.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ("campusonline", "0078_linz"),
+        ("campusonline", "0001_initial"),
     ]
 
     operations = [
